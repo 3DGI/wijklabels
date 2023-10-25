e3a62df3991da3f6962d0fb708db8b4f6c4a4de9
Balázs Dukai <balazs.dukai@gmail.com>
2023-10-25 17:33:59 +0200

    Select oppervlakte from the BAG

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ You need [just](https://just.systems/) to set up the test data files.
 
 Initialize the test data with `just download`.
 This will download the test data files.
+
+## Notes
+
+What does it mean when a VBO related to multiple Pand (pandref)?
+
+How to deal with the hoofd- and nevenadresnummer in VBO?
---
+++ src/wijklabels/sql/collect_verblijfsobject.sql
@@ -0,0 +1,36 @@
+WITH vbo AS (SELECT identificatie AS vbo_identificatie
+                  , gebruiksdoel
+                  , oppervlakte
+                  , pandref
+                  , hoofdadresnummeraanduidingref
+                  , nevenadresnummeraanduidingref
+             FROM lvbag.verblijfsobjectactueelbestaand
+             WHERE 'woonfunctie' = ANY (gebruiksdoel))
+   , nma AS (SELECT identificatie AS nma_identificatie
+                  , huisnummer
+                  , huisletter
+                  , huisnummertoevoeging
+                  , postcode
+                  , typeadresseerbaarobject
+             FROM lvbag.nummeraanduidingactueelbestaand)
+   , vbo_nma AS (SELECT *
+                 FROM vbo
+                          LEFT JOIN nma ON vbo.hoofdadresnummeraanduidingref =
+                                           nma.nma_identificatie)
+   , vbo_nma_pd AS (SELECT vbo_nma.vbo_identificatie
+                         , pd.identificatie AS pd_identificatie
+                         , huisnummer
+                         , huisletter
+                         , huisnummertoevoeging
+                         , postcode
+                         , gebruiksdoel
+                         , oppervlakte
+                    FROM vbo_nma
+                             LEFT JOIN lvbag.pandactueelbestaand AS pd
+                                       ON vbo_nma.pandref[1] = pd.identificatie
+                    WHERE vbo_nma.hoofdadresnummeraanduidingref IS NOT NULL)
+SELECT *
+FROM vbo_nma_pd
+WHERE pd_identificatie IS NOT NULL
+  AND postcode IS NOT NULL
+ORDER BY pd_identificatie;
