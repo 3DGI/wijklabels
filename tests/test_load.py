b6f1e2843914655e671681a3d6200225c1f20c06
Balázs Dukai <balazs.dukai@gmail.com>
2023-11-08 14:09:49 +0100

    Fix tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,8 +4,8 @@ from wijklabels.load import CityJSONLoader, VBOLoader
 def test_cityjsonloader_files(data_dir):
     path_1 = data_dir / "9-316-552.city.json"
     path_2 = data_dir / "9-316-556.city.json"
-    cmloader = CityJSONLoader()
-    cm = cmloader.load(files=[path_1, path_2])
+    cmloader = CityJSONLoader(files=[path_1, path_2])
+    cm = cmloader.load()
     print(cm)
 
 
@@ -13,8 +13,8 @@ def test_cityjsonloader_files_bbox(data_dir):
     path_1 = data_dir / "9-316-552.city.json"
     path_2 = data_dir / "9-316-556.city.json"
     bbox = (92892, 445701, 93328, 446094)
-    cmloader = CityJSONLoader()
-    cm = cmloader.load(files=[path_1, path_2], bbox=bbox)
+    cmloader = CityJSONLoader(files=[path_1, path_2], bbox=bbox)
+    cm = cmloader.load()
     print(cm)
 
 
--- tests/test_vormfactor.py
+++ tests/test_vormfactor.py
@@ -4,19 +4,20 @@ from wijklabels.vormfactor import verliesoppervlakte, vormfactor
 
 def test_verliesoppervlakte(data_dir):
     p = data_dir / "one.city.json"
-    cmloader = CityJSONLoader()
-    cm = cmloader.load(files=[p, ])
+    cmloader = CityJSONLoader(files=[p, ])
+    cm = cmloader.load()
     for coid, co in cm.j["CityObjects"].items():
         print(f"Computing verliesoppervlak of {coid}")
         res = verliesoppervlakte(co)
         print(res)
 
 
-def test_vormfactor(data_dir):
+def test_vormfactor(data_dir, vbo_df):
     p = data_dir / "one.city.json"
-    cmloader = CityJSONLoader()
-    cm = cmloader.load(files=[p, ])
+    cmloader = CityJSONLoader(files=[p, ])
+    cm = cmloader.load()
     for coid, co in cm.j["CityObjects"].items():
         print(f"Computing vormfactor of {coid}")
-        res = vormfactor(co)
+        res = vormfactor(cityobject_id=coid, cityobject=co, vbo_df=vbo_df,
+                         floor_area=True)
         print(res)
