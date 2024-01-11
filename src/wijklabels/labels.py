7b2b0926d9d83f8bbe77182de853dccff0009b85
Balázs Dukai <balazs.dukai@gmail.com>
2024-01-11 10:54:03 +0100

    Label test within

--- src/wijklabels/labels.py
+++ src/wijklabels/labels.py
@@ -40,6 +40,17 @@ class EnergyLabel(OrderedEnum):
     def __repr__(self):
         return self.__str__()
 
+    def within(self, other, range: int):
+        """Test if the other label is within the provided range of labels.
+        For example, the labels C, B, A are within the range of 1 of the
+        label B. The labels D, C, B, A, A+ are within the range of 2 of the label B.
+        The label B is in the range of 0 of the label B.
+        The label-list is not recycled for the range, thus for the label G, the range of
+        2 are G, F, E.
+        """
+        member_list = self.__class__._member_list()
+        return member_list.index(self) - range <= member_list.index(other) <= member_list.index(self) + range
+
 
 def parse_energylabel_ditributions(excelloader) -> LabelDistributions:
     """Parse the energy label distributions from the excel file.
--- tests/test_labels.py
+++ tests/test_labels.py
@@ -1,4 +1,6 @@
-from wijklabels.labels import parse_energylabel_ditributions, reshape_for_classification
+from pytest import mark
+from wijklabels.labels import parse_energylabel_ditributions, \
+    reshape_for_classification, EnergyLabel
 
 
 def test_parse_energy_label_distributions(excelloader):
@@ -10,3 +12,22 @@ def test_reshape_for_classification(excelloader):
     label_distributions = parse_energylabel_ditributions(excelloader)
     res = reshape_for_classification(label_distributions)
     print(res)
+
+
+@mark.parametrize(
+    ("_self", "other", "range", "result"),
+    (
+            (EnergyLabel.B, EnergyLabel.B, 0, True),
+            (EnergyLabel.B, EnergyLabel.A, 0, False),
+            (EnergyLabel.B, EnergyLabel.A, 1, True),
+            (EnergyLabel.B, EnergyLabel.C, 1, True),
+            (EnergyLabel.B, EnergyLabel.B, 1, True),
+            (EnergyLabel.B, EnergyLabel.D, 1, False),
+            (EnergyLabel.B, EnergyLabel.APPP, 1, False),
+            (EnergyLabel.G, EnergyLabel.E, 2, True),
+            (EnergyLabel.G, EnergyLabel.APPPP, 2, False),
+            (EnergyLabel.APPPP, EnergyLabel.G, 2, False),
+    )
+)
+def test_within(_self, other, range, result):
+    assert _self.within(other, range) == result
